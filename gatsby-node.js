4f2dd6955b3e76a8843081aeb7660b1075942afc
Carl Vitullo <carl@stellar.org>
2019-10-09 16:26:36 -0400

    WIP

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,13 @@
 const path = require("path");
 
+const stringify = (x) => JSON.stringify(x, null, 2);
+const log = (...args) =>
+  console.log("[folder-metadata]", ...args.map(stringify));
+const warn = (...args) =>
+  console.warn("[folder-metadata]", ...args.map(stringify));
+const error = (...args) =>
+  console.error("[folder-metadata]", ...args.map(stringify));
+
 // The metadata is keyed off the relative path of the containing folder. Its
 // contents are the complete contents of `metadata.json`, plus a `parent` key
 // that contains any content from 1 folder up.
@@ -8,9 +16,9 @@ const metadataLookup = {};
 const isParentFolder = (folder, maybeParent) =>
   path.relative(folder, maybeParent) === "..";
 
-const buildMetadata = (file, metadata) => {
+const buildMetadata = ({ file, instance, metadata }) => {
   // The find will get a [path, data] tuple or null
-  const parent = Object.entries(metadataLookup).find(([path]) =>
+  const parent = Object.entries(metadataLookup[instance]).find(([path]) =>
     isParentFolder(file.relativeDirectory, path),
   ) || [null, null];
   return {
@@ -19,46 +27,71 @@ const buildMetadata = (file, metadata) => {
   };
 };
 
-const fillChildMetadata = (path, metadata) => {
-  Object.entries(metadataLookup).forEach(([maybeChild, data]) => {
+const noteFileExists = ({ file, instance }) =>
+  buildMetadata({ file, instance, metadata: null });
+
+const fillChildMetadata = ({ path, instance, metadata }) => {
+  Object.entries(metadataLookup[instance]).forEach(([maybeChild, data]) => {
     if (isParentFolder(maybeChild, path) && !data.parent) {
       data.parent = metadata;
-      console.log({ data });
     }
   });
 };
 
+const defaultIgnoredTypes = ["Directory"];
+
 exports.onCreateNode = ({ node, actions }, options = {}) => {
+  const ignoredNodeTypes = options.ignore || defaultIgnoredTypes;
+  if (ignoredNodeTypes.includes(node.internal.type)) return;
+
   // We need the absolute path so we can correctly `require()` these json
   // files, and the relative directory so we can compare their locations sans
   // details like .cache/ or the filename.
-  const { absolutePath, relativeDirectory: dir } = node;
-  if (!absolutePath || !dir) {
+  const {
+    absolutePath,
+    relativeDirectory,
+    sourceInstanceName: instance,
+    base,
+  } = node;
+  const dir = `./${relativeDirectory}`;
+  if (absolutePath === undefined || relativeDirectory === undefined) {
     return;
   }
+  if (!metadataLookup[instance]) {
+    metadataLookup[instance] = {};
+  }
 
-  const metadataFilename = options.filename || "metadata.json";
+  const metadataFilename = options.metadataFilename || "metadata.json";
 
-  if (node.base === metadataFilename) {
+  if (base === metadataFilename) {
     const json = require(absolutePath);
-    const metadata = buildMetadata(node, json);
+    const metadata = buildMetadata({ file: node, instance, metadata: json });
     // Nodes aren't added strictly from child -> parent, so we need to backfill
     // any child folders that have already been processed.
-    fillChildMetadata(dir, metadata);
-    metadataLookup[dir] = metadata;
+    fillChildMetadata({ path: dir, instance, metadata });
+    metadataLookup[instance][dir] = {
+      ...metadataLookup[instance][dir],
+      ...metadata,
+    };
+    // log("building", { instance, dir, metadata, base });
     return;
   }
 
-  const foundMetadata = metadataLookup[dir];
+  const foundMetadata = metadataLookup[instance][dir];
   if (foundMetadata) {
+    log("adding metadata to ", { instance, dir, base });
     actions.createNodeField({
       node,
       name: "metadata",
       value: foundMetadata,
     });
+  } else {
+    const newMetadata = buildMetadata({ file: node, instance, metadata: null });
+    metadataLookup[instance][dir] = newMetadata;
+    actions.createNodeField({
+      node,
+      name: "metadata",
+      value: newMetadata,
+    });
   }
-};
-
-exports.onPostBootstrap = () => {
-  console.log(metadataLookup);
 };
