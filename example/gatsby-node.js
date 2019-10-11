4d9b29febc27a8bea819a3dc4e6ba4d1ee2e6b7d
Carl Vitullo <carl@stellar.org>
2019-10-11 10:49:45 -0400

    Add build test

--- example/gatsby-node.js
+++ example/gatsby-node.js
@@ -1,15 +1,24 @@
+const deepEqual = require("deep-equal")
 const { SOURCES } = require("./sources")
+const childFixture = require("./fixtures/child")
+const parentFixture = require("./fixtures/parent")
+const completeFixture = require("./fixtures/complete")
 
-const assert = (message, predicate) => {
-  if (!predicate()) throw new Error(``)
+const errors = []
+
+const assert = (condition, message) => {
+  if (!condition) {
+    errors.push(message)
+  }
 }
 
 const getFilesBySource = (graphql, source) =>
   graphql(`
     {
-      allFile({filter: {sourceInstanceName: {eq: "${source}"}}}) {
+      allFile(filter: {sourceInstanceName: {eq: "${source}"}}) {
         edges {
           node {
+            base
             relativeDirectory
             fields {
               metadata {
@@ -17,6 +26,12 @@ const getFilesBySource = (graphql, source) =>
                   data {
                     id
                     title
+                  }
+                  parent {
+                    data {
+                      id
+                      title
+                    }
                   }
                 }
                 data {
@@ -35,10 +50,28 @@ const getFilesBySource = (graphql, source) =>
 //   const root = complete.find(m => m.relativeDirectory)
 // }
 
+const stringify = x => JSON.stringify(x, null, 2)
 exports.createPages = ({ graphql, actions }) => {
   return Promise.all([
     getFilesBySource(graphql, SOURCES.childOnly),
     getFilesBySource(graphql, SOURCES.parentOnly),
     getFilesBySource(graphql, SOURCES.completeMetadata),
-  ]).then(([childOnly, parentOnly, completeMetadata]) => {})
+  ]).then(([childOnly, parentOnly, completeMetadata]) => {
+    assert(deepEqual(childOnly, childFixture), "child data must be deep equal")
+    assert(
+      deepEqual(parentOnly, parentFixture),
+      "parent data must be deep equal"
+    )
+    assert(
+      deepEqual(completeMetadata, completeFixture),
+      "complete data must be deep equal"
+    )
+
+    if (errors.length > 0) {
+      errors.forEach(x => console.error(x))
+      throw new Error(
+        `Some tests failed, output must exactly match fixture data`
+      )
+    }
+  })
 }
