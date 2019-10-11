f0339f7acc0411c22bbf9f1bca41d4ff14d4605b
Carl Vitullo <carl@stellar.org>
2019-10-11 10:49:32 -0400

    Fix logic so it consistently works

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,18 @@
 const path = require("path");
 
-const stringify = (x) => JSON.stringify(x, null, 2);
-const log = (...args) =>
-  console.log("[folder-metadata]", ...args.map(stringify));
-const warn = (...args) =>
-  console.warn("[folder-metadata]", ...args.map(stringify));
-const error = (...args) =>
-  console.error("[folder-metadata]", ...args.map(stringify));
-
 // The metadata is keyed off the relative path of the containing folder. Its
 // contents are the complete contents of `metadata.json`, plus a `parent` key
 // that contains any content from 1 folder up.
+const rawMetadata = {};
 const metadataLookup = {};
+const allReferencedNodes = [];
 
 const isParentFolder = (folder, maybeParent) =>
   path.relative(folder, maybeParent) === "..";
 
-const buildMetadata = ({ file, instance, metadata }) => {
-  // The find will get a [path, data] tuple or null
-  const parent = Object.entries(metadataLookup[instance]).find(([path]) =>
-    isParentFolder(file.relativeDirectory, path),
+const buildMetadata = ({ node, instance, metadata }) => {
+  const parent = Object.entries(metadataLookup[instance]).find(([directory]) =>
+    isParentFolder(node.relativeDirectory, directory),
   ) || [null, null];
   return {
     data: metadata,
@@ -27,22 +20,11 @@ const buildMetadata = ({ file, instance, metadata }) => {
   };
 };
 
-const noteFileExists = ({ file, instance }) =>
-  buildMetadata({ file, instance, metadata: null });
-
-const fillChildMetadata = ({ path, instance, metadata }) => {
-  Object.entries(metadataLookup[instance]).forEach(([maybeChild, data]) => {
-    if (isParentFolder(maybeChild, path) && !data.parent) {
-      data.parent = metadata;
-    }
-  });
-};
-
-const defaultIgnoredTypes = ["Directory"];
+const includeNodeTypes = ["File"];
 
 exports.onCreateNode = ({ node, actions }, options = {}) => {
-  const ignoredNodeTypes = options.ignore || defaultIgnoredTypes;
-  if (ignoredNodeTypes.includes(node.internal.type)) return;
+  const nodeTypes = options.includeNodeTypes || includeNodeTypes;
+  if (!nodeTypes.includes(node.internal.type)) return;
 
   // We need the absolute path so we can correctly `require()` these json
   // files, and the relative directory so we can compare their locations sans
@@ -53,45 +35,59 @@ exports.onCreateNode = ({ node, actions }, options = {}) => {
     sourceInstanceName: instance,
     base,
   } = node;
-  const dir = `./${relativeDirectory}`;
   if (absolutePath === undefined || relativeDirectory === undefined) {
     return;
   }
-  if (!metadataLookup[instance]) {
-    metadataLookup[instance] = {};
+  const dir = `./${relativeDirectory}`;
+
+  if (!rawMetadata[instance]) {
+    rawMetadata[instance] = {};
   }
 
+  // Load all metadata. We don't need to add metadata to metadata files, so don't
+  // add them to the list of file nodes.
   const metadataFilename = options.metadataFilename || "metadata.json";
-
   if (base === metadataFilename) {
     const json = require(absolutePath);
-    const metadata = buildMetadata({ file: node, instance, metadata: json });
-    // Nodes aren't added strictly from child -> parent, so we need to backfill
-    // any child folders that have already been processed.
-    fillChildMetadata({ path: dir, instance, metadata });
-    metadataLookup[instance][dir] = {
-      ...metadataLookup[instance][dir],
-      ...metadata,
-    };
-    // log("building", { instance, dir, metadata, base });
-    return;
+    rawMetadata[instance][dir] = json;
   }
 
-  const foundMetadata = metadataLookup[instance][dir];
-  if (foundMetadata) {
-    log("adding metadata to ", { instance, dir, base });
+  allReferencedNodes.push(node);
+};
+
+const getNodeInfo = ({ relativeDirectory, sourceInstanceName: instance }) => ({
+  dir: `./${relativeDirectory}`,
+  instance,
+});
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const sortedNodes = allReferencedNodes.sort((a, b) => {
+    const aLength = a.absolutePath.split(path.sep).length;
+    const bLength = b.absolutePath.split(path.sep).length;
+    return aLength - bLength;
+  });
+  sortedNodes.forEach((node) => {
+    const { dir, instance } = getNodeInfo(node);
+    if (!metadataLookup[instance]) {
+      metadataLookup[instance] = {};
+    }
+    metadataLookup[instance][dir] = buildMetadata({
+      node,
+      instance,
+      metadata: rawMetadata[instance][dir],
+    });
+  });
+  sortedNodes.forEach((node) => {
+    const { dir, instance } = getNodeInfo(node);
+
     actions.createNodeField({
       node,
       name: "metadata",
-      value: foundMetadata,
+      value: buildMetadata({
+        node,
+        instance,
+        metadata: rawMetadata[instance][dir],
+      }),
     });
-  } else {
-    const newMetadata = buildMetadata({ file: node, instance, metadata: null });
-    metadataLookup[instance][dir] = newMetadata;
-    actions.createNodeField({
-      node,
-      name: "metadata",
-      value: newMetadata,
-    });
-  }
+  });
 };
